3987baa4bde0cfaebfcd60bbbb27b66dcf8b244d
Nirmalya Ghosh <nirmalya.email@gmail.com>
2016-07-21 02:04:16 +0530

    refactored the validations for the new card form

--- app/components/forms/new-card-form.js
+++ app/components/forms/new-card-form.js
@@ -64,24 +64,27 @@ export default Ember.Component.extend({
     },
 
     validateInputTitle(data) {
-      this.set('errors.title', 'Title is required!');
-
       if (data) {
+        this.set('errors.title', '');
+
         $('#card-title').removeClass('form-control-danger').addClass('form-control-success');
         $('#card-title').closest('.form-group').removeClass('has-danger').addClass('has-success');
       } else {
+        this.set('errors.title', 'Title is required!');
+
         $('#card-title').removeClass('form-control-success').addClass('form-control-danger');
         $('#card-title').closest('.form-group').removeClass('has-success').addClass('has-danger');
       }
     },
 
     validateInputDescription(data) {
-      this.set('errors.description', 'Description is required!');
-
       if (data) {
+        this.set('errors.description', '');
+
         $('#card-description').removeClass('form-control-danger').addClass('form-control-success');
         $('#card-description').closest('.form-group').removeClass('has-danger').addClass('has-success');
       } else {
+        this.set('errors.description', 'Description is required!');
         $('#card-description').removeClass('form-control-success').addClass('form-control-danger');
         $('#card-description').closest('.form-group').removeClass('has-success').addClass('has-danger');
       }
--- dist/assets/client.js
+++ dist/assets/client.js
@@ -262,24 +262,27 @@ define('client/components/forms/new-card-form', ['exports', 'ember'], function (
       },
 
       validateInputTitle: function validateInputTitle(data) {
-        this.set('errors.title', 'Title is required!');
-
         if (data) {
+          this.set('errors.title', '');
+
           $('#card-title').removeClass('form-control-danger').addClass('form-control-success');
           $('#card-title').closest('.form-group').removeClass('has-danger').addClass('has-success');
         } else {
+          this.set('errors.title', 'Title is required!');
+
           $('#card-title').removeClass('form-control-success').addClass('form-control-danger');
           $('#card-title').closest('.form-group').removeClass('has-success').addClass('has-danger');
         }
       },
 
       validateInputDescription: function validateInputDescription(data) {
-        this.set('errors.description', 'Description is required!');
-
         if (data) {
+          this.set('errors.description', '');
+
           $('#card-description').removeClass('form-control-danger').addClass('form-control-success');
           $('#card-description').closest('.form-group').removeClass('has-danger').addClass('has-success');
         } else {
+          this.set('errors.description', 'Description is required!');
           $('#card-description').removeClass('form-control-success').addClass('form-control-danger');
           $('#card-description').closest('.form-group').removeClass('has-success').addClass('has-danger');
         }
