18a72d50a311f0706cc58c1f3ae4fdebed0e4f7c
Nirmalya Ghosh <nirmalya.email@gmail.com>
2017-01-12 17:14:23 +0530

    fix(bootstrap.css.map): remove the importing of bootstrap.css.map

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -44,7 +44,6 @@ module.exports = function(defaults) {
   app.import('bower_components/tether/dist/js/tether.min.js');
   app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
   app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css.map');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
